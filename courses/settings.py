52f20245a201f6f8f6c8fd15548840f72732885b
Oksana <oks.tym@gmail.com>
2017-05-26 01:44:24 +0300

    Static root changed

--- courses/settings.py
+++ courses/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 STATIC_URL = '/static/'
 
