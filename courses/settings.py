3ec7d40fbe36261157a0770dfeb574da89679e24
Oksana <oks.tym@gmail.com>
2017-05-26 14:06:36 +0300

    db dump added

--- courses/settings.py
+++ courses/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -26,9 +27,6 @@ SECRET_KEY = 'pprduan4x*$nm#vz%nq^-r3rjo11#x%rhh6m+sxq5q@di5ht$4'
 DEBUG = True
 
 ALLOWED_HOSTS = ['shrouded-crag-79222.herokuapp.com']
-
-import dj_database_url
-
 
 # Application definition
 
@@ -76,18 +74,23 @@ WSGI_APPLICATION = 'courses.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'course',
+#         'USER': 'postgres',
+#         'PASSWORD': '1234',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'course',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
     }
 }
-
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
