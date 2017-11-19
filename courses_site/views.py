7636a9855ef39e7ca8dee45082b67162252e981e
OksanaTymchenko <oks.tym@gmail.com>
2017-11-19 23:12:56 +0200

    letter

--- courses_site/views.py
+++ courses_site/views.py
@@ -274,9 +274,9 @@ def search(request):
 
 def emailing(request):
     emails = [u.email for u in User.objects.all() if len(u.email) > 0]
-    subject, from_email, to = 'New courses are added', 'lucyuk.a.v@gmail.com', emails
+    subject, from_email, to = 'New courses are added', 'moocsources@gmail.com', emails
     text_content = 'New courses are added'
-    f = open('/Volumes/MemoryCard/4c2s/courses/static/letter.html')
+    f = open('./static/letter.html')
     html_content = ''.join(f.readlines())
     print(len(html_content))
     courses = Course.objects.all()[:3]
